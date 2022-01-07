035219ec8db5ccd1e19cdfc9eb1fe4cd43bd126b
armaBurton <armanarma@armaburton.com>
2022-01-06 19:53:52 -0800

    init commit

---
+++ list/script/list.js
@@ -0,0 +1,79 @@
+import { 
+    checkAuth,
+    logout,
+    createListItem,
+    getListItems,
+    togglePurchased,
+    deleteList
+} from '../../script/fetch-utils.js';
+
+const logoutButton = document.getElementById(`logout`);
+const deleteButton = document.getElementById(`delete-list`);
+const form = document.getElementById(`form`);
+const renderedList = document.getElementById(`rendered-list`);
+
+checkAuth();
+
+form.addEventListener(`submit`, async(e) =>{
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    const quantity = data.get(`quantity-input`);
+    const item = data.get(`item-input`);
+    
+    const qInput = document.getElementById(`quantity-input`);
+    qInput.value = '';
+    const iInput = document.getElementById(`item-input`);
+    iInput.value = '';
+
+    await createListItem(quantity, item);
+
+    await fetchAndDisplayList();
+});
+
+logoutButton.addEventListener(`click`, () => {
+    logout();
+});
+
+deleteButton.addEventListener(`click`, async() => {
+    await deleteList();
+
+    await fetchAndDisplayList();
+});
+
+async function fetchAndDisplayList(){
+    const list = await getListItems();
+
+    renderedList.textContent = '';
+    
+    for (let l of list){
+
+        const div = document.createElement(`div`);
+        div.classList.add(`item-div`, `flex-row`, `pad-left`);
+        
+        div.addEventListener(`click`, async() => {
+            div.classList.remove(`item-div`);
+            div.classList.add(`item-div-purchased`);
+            
+            await togglePurchased(l.id);
+        });
+
+        const quantity = document.createElement(`p`);
+        quantity.classList.add(`item-quantity`);
+        quantity.textContent = l.quantity;
+
+        const item = document.createElement(`p`);
+        item.classList.add(`list-item`);
+        item.textContent = l.item;
+
+        div.append(quantity, item);
+
+        renderedList.append(div);
+    }
+}
+
+window.addEventListener(`load`, async() => {
+    await fetchAndDisplayList();
+});
+
---
+++ script/fetch-utils.js
@@ -0,0 +1,83 @@
+const SUPABASE_URL = 'https://zbycmansaybtrruijgxp.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTUxMTkyNiwiZXhwIjoxOTU3MDg3OTI2fQ.Ni09YI3wnnjziemmdKAzYVvr69wg_bpWKc2CZVgzl_M';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+
+export async function getUser() {
+    return client.auth.session();
+}
+
+export async function createListItem(quantity, item){
+    const response = await client
+        .from(`list`)
+        .insert([
+            {
+                quantity,
+                item,
+                purchased: false
+            }
+        ]);
+    
+    return checkError(response);
+}
+
+export async function togglePurchased(id){
+    console.log(id);
+    const response = await client
+        .from(`list`)
+        .update({ purchased: true })
+        .match({ id });
+
+    return checkError(response);
+}
+
+export async function getListItems(){
+    const response = await client
+        .from(`list`)
+        .select();
+
+    return checkError(response);
+}
+
+export async function deleteList(){
+    const response = await client
+        .from(`list`)
+        .delete();
+
+    return checkError(response);
+}
+
+export async function checkAuth() {
+    const user = await getUser();
+
+    if (!user) location.replace('../'); 
+}
+
+export async function redirectIfLoggedIn() {
+    if (await getUser()) {
+        location.replace('./list');
+    }
+}
+
+export async function signUpUser(email, password){
+    const response = await client.auth.signUp({ email, password });
+    
+    return response.user;
+}
+
+export async function signInUser(email, password){
+    const response = await client.auth.signIn({ email, password });
+
+    return response.user;
+}
+
+export async function logout() {
+    await client.auth.signOut();
+
+    return window.location.href = '../';
+}
+
+function checkError({ data, error }) {
+    return error ? console.error(error) : data;
+}
